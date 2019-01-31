6153dbe29cf35b910c4ba5a132a052633238a3de
irtaimal <ir.taimal10@uniandes.edu.co>
2019-01-31 17:12:13 -0500

    F1: Agregue el código necesario para inyectar datos en el primer reload del cliente (provenientes del API mencionado anteriormente). Recuerde diferenciar el primer reload al resto.

--- js/main.js
+++ js/main.js
@@ -5,42 +5,55 @@ $(function () {
         $options_wrapper = $('#options_wrapper');
 
     var stations = [];
+    var db = new PouchDB('stations');
 
 
     var getStationSchedules = function (stationSlug) {
-        var schedules_url = `https://api-ratp.pierre-grimaud.fr/v3/schedules/rers/b/${stationSlug}/A`;
-        var traffic_url = 'https://api-ratp.pierre-grimaud.fr/v3/traffic/rers/b';
-        $.when(
-            $.getJSON(schedules_url),
-            $.getJSON(traffic_url))
-            .done(function (schedules, traffic) {
-                var date = new Date(),
-                    hours = date.getHours(),
-                    minutes = date.getMinutes(),
-                    current_time = (hours < 10 ? '0' + hours : hours) + ':' + (minutes < 10 ? '0' + minutes : minutes);
 
-                 var selectedStation = stations.filter(function (item) {
-                    return item.slug === stationSlug;
-                })[0];
+        var selectedStation = stations.filter(function (item) {
+            return item.slug === stationSlug;
+        })[0];
 
+        if (selectedStation.info) {
+            $content.html(template(selectedStation.info));
+        } else {
+            var schedules_url = `https://api-ratp.pierre-grimaud.fr/v3/schedules/rers/b/${stationSlug}/A`;
+            var traffic_url = 'https://api-ratp.pierre-grimaud.fr/v3/traffic/rers/b';
+            $.when(
+                $.getJSON(schedules_url),
+                $.getJSON(traffic_url))
+                .done(function (schedules, traffic) {
+                    var date = new Date(),
+                        hours = date.getHours(),
+                        minutes = date.getMinutes(),
+                        current_time = (hours < 10 ? '0' + hours : hours) + ':' + (minutes < 10 ? '0' + minutes : minutes);
 
-                var trafficResponse = traffic[0].result,
-                    scheduleResponse = schedules[0].result;
+                    var trafficResponse = traffic[0].result,
+                        scheduleResponse = schedules[0].result;
 
-                var data = {
-                    traffic: trafficResponse.message,
-                    line: 'B',
-                    type: 'rer',
-                    horaires: scheduleResponse.schedules,
-                    destination: 'Charles De Gaulle - Mitry Claye',
-                    station: selectedStation.name,
-                    current_time: current_time
-                };
-                console.log('schedules', schedules);
-                console.log('traffic', traffic);
+                    var data = {
+                        _id: selectedStation.slug,
+                        traffic: trafficResponse.message,
+                        line: 'B',
+                        type: 'rer',
+                        horaires: scheduleResponse.schedules,
+                        destination: 'Charles De Gaulle - Mitry Claye',
+                        station: selectedStation.name,
+                        current_time: current_time
+                    };
 
-                $content.html(template(data));
-            });
+                    db.get(selectedStation.slug).then(function (doc) {
+                        doc.info = data;
+                        return db.put(doc);
+                    }).then(function (response) {
+                        // handle response
+                    }).catch(function (err) {
+                        console.log(err);
+                    });
+
+                    $content.html(template(data));
+                });
+        }
     };
 
     var addOptionsEvents = function () {
@@ -56,6 +69,15 @@ $(function () {
             $.getJSON(stations_url))
             .done(function (response) {
                 stations = response.result.stations;
+                response.result.stations.forEach(station => {
+                    station._id = station.slug;
+                    db.put(station, function callback(err, result) {
+                        if (!err) {
+                            console.log('Successfully posted a todo!');
+                        }
+                    });
+                });
+                getStationSchedules(stations[16].slug);
                 $options_wrapper.html(options_template({
                     stations: stations
                 }));
@@ -64,6 +86,26 @@ $(function () {
     };
 
 
-    getStations();
-    getStationSchedules('arcueil+cachan');
+    db.allDocs({include_docs: true, descending: true}, function (err, doc) {
+        if (doc.rows.length > 0) {
+            stations = doc.rows.map(r => r.doc);
+            getStationSchedules(stations[16].slug);
+            $options_wrapper.html(options_template({
+                stations: stations
+            }));
+            addOptionsEvents();
+        } else {
+            getStations();
+        }
+    });
+
+
+    // TODO add service worker code here
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register('./service-worker.js')
+            .then(function () {
+                console.log('Service Worker Registered');
+            });
+    }
 });
