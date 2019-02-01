7a96e3427b469fadffbb18c46e20f97734910d8f
irtaimal <ir.taimal10@uniandes.edu.co>
2019-01-31 19:17:28 -0500

    F4: Utilice el ServiceWorker para guardar en cache los datos de los horarios de cada estación.

--- service-worker.js
+++ service-worker.js
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var dataCacheName = 'weatherData-v1';
-var cacheName = 'weatherPWA-final-1';
+var dataCacheName = 'DataMetro-v1';
+var cacheName = 'DataMetro-final-1';
 var filesToCache = [
     '/',
     '/index.html',
